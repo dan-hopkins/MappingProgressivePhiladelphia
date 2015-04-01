e0e4afd3495aed5ef6f04ff4fcf9a7156cde1304
Evan Hamilton <evanphamilton@gmail.com>
2015-03-31 21:10:57 -0400

    Syncing Picasso and trying to display pictures

--- app/src/main/java/edu/haverford/mpp/mappingprogressivephiladelphia/Facebook_Login.java
+++ app/src/main/java/edu/haverford/mpp/mappingprogressivephiladelphia/Facebook_Login.java
@@ -1,23 +1,18 @@
 package edu.haverford.mpp.mappingprogressivephiladelphia;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.facebook.HttpMethod;
-import com.facebook.Request;
-import com.facebook.Response;
-import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.UiLifecycleHelper;
-import com.facebook.model.GraphUser;
-import com.facebook.widget.LoginButton;
-
-import java.util.Arrays;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by evanhamilton on 3/22/15.
@@ -25,75 +20,61 @@ import java.util.Arrays;
 public class Facebook_Login extends FragmentActivity{
     TextView userInfoTextView;
 
+    public String id;
+    public String url;
 
-    private UiLifecycleHelper uiHelper;
-    private Session.StatusCallback callback = new Session.StatusCallback() {
-        @Override
-        public void call(Session session, SessionState state, Exception exception) {
-            onSessionStateChange(session, state, exception);
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("Created", "created");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.facebook_login);
-        uiHelper = new UiLifecycleHelper(this, callback);
-        uiHelper.onCreate(savedInstanceState);
-        LoginButton button = (LoginButton) findViewById(R.id.authButton);
-        button.setReadPermissions(Arrays.asList("basic_info", "email"));
+        Firebase.setAndroidContext(this);
+        Firebase myFirebaseRef = new Firebase("https://mappingprogressiveph.firebaseio.com/");
+        myFirebaseRef.child("Out4STEM1").addValueEventListener(new ValueEventListener() {
 
-        userInfoTextView = (TextView) findViewById(R.id.userInfoTextView);
+
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                //Log.d("string", "string");
+                //System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+                id = snapshot.child("Facebook_ID").getValue().toString();
+                System.out.println(id);
+
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                url = new String("https://graph.facebook.com/" + id + "/picture?type=large");
+                Log.d(url, "URL");
+
+                Picasso.with(getBaseContext())
+                        .load(url)
+                        .error(R.drawable.default_pic)
+                        .into(imageView);
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError error) {
+            }
+
+
+        });
+
+        /*
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        url = new String("https://graph.facebook.com/"+id+"/picture?type=large");
+        Log.d(url, "URL");
+
+        Picasso.with(this)
+                .load(url)
+                .error(R.drawable.default_pic)
+                .into(imageView);
+        */
 
 
 
     }
 
-    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
-        if (session != null && session.isOpened()) {
 
-            Log.d("DEBUG", "facebook session is open ");
-
-            new Request(
-                    session,
-                    "/418228184955872",
-                    null,
-                    HttpMethod.GET,
-                    new Request.Callback() {
-                        public void onCompleted(Response response) {
-                            /* handle the result */
-                            userInfoTextView.append(response.toString());
-                        }
-                    }
-            ).executeAsync();
-
-            // make request to the /me API
-            Request.newMeRequest(session, new Request.GraphUserCallback() {
-
-                // callback after Graph API response with user object
-                @Override
-                public void onCompleted(GraphUser user, Response response) {
-                    Log.d("Working", "onComplete");
-                    if (user != null) {
-                        userInfoTextView.append(user.getFirstName().toString());
-
-                    }
-                }
-
-
-            }).executeAsync();
-
-        }
-        else if (session.isClosed()) {
-            userInfoTextView.append("Delete");
-        }
-    }
-
-
-
-
-
+ 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -101,37 +82,10 @@ public class Facebook_Login extends FragmentActivity{
         return true;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        uiHelper.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        uiHelper.onPause();
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        uiHelper.onActivityResult(requestCode, resultCode, data);
-    }
 
 
 
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        uiHelper.onDestroy();
-    }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        uiHelper.onSaveInstanceState(outState);
-    }
 
 }
