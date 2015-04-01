5bed6b5bfcc5f0187c728fdad3a716bf791623e2
Brian Guggenheimer <bguggs@gmail.com>
2015-03-31 20:17:26 -0400

    Added static BMP methods

---
+++ app/src/main/java/edu/haverford/mpp/mappingprogressivephiladelphia/MPPUtil.java
@@ -0,0 +1,58 @@
+package edu.haverford.mpp.mappingprogressivephiladelphia;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+/**
+ * Created by BrianG on 3/31/2015.
+ */
+public class MPPUtil {
+
+    public static String saveToInternalSorage(Bitmap bitmapImage, Context context, int ID, String directoryName){
+        ContextWrapper cw = new ContextWrapper(context);
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir(directoryName, Context.MODE_PRIVATE);
+        // Create imageDir
+        File path = new File(directory,"mpp_profile_pic_" + Integer.toString(ID)); //
+
+        FileOutputStream fos = null;
+        try {
+
+            fos = new FileOutputStream(path);
+
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return directory.getAbsolutePath();
+    }
+
+    public static Bitmap loadImageFromStorage(String path)
+    {
+
+        try {
+            File f=new File(path, "profile.jpg");
+            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+            return b;
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+
+
+}
