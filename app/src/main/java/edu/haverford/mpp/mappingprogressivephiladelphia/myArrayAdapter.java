5396282d249eeb4d6842092678327c31344467e5
Evan Hamilton <evanphamilton@gmail.com>
2015-05-08 15:19:12 -0400

    Better item styling

--- app/src/main/java/edu/haverford/mpp/mappingprogressivephiladelphia/myArrayAdapter.java
+++ app/src/main/java/edu/haverford/mpp/mappingprogressivephiladelphia/myArrayAdapter.java
@@ -1,31 +1,16 @@
 package edu.haverford.mpp.mappingprogressivephiladelphia;
 
-import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-
-
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Filter;
-import android.widget.Filterable;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.gms.analytics.ecommerce.Product;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.zip.Inflater;
 
 
 public class myArrayAdapter extends ArrayAdapter {
@@ -50,7 +35,7 @@ public class myArrayAdapter extends ArrayAdapter {
 
             ImageView myImageView = (ImageView) view.findViewById(R.id.org_pic);
             Picasso.with(mContext)
-                    .load("https://graph.facebook.com/" + po.getFacebookID() + "/picture?type=large")
+                    .load("https://graph.facebook.com/" + po.getFacebookID() + "/picture?width=99999")
                     .placeholder(R.drawable.default_pic)
                     .into(myImageView);
         }
